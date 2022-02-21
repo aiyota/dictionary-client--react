067fc10bb922a17d24d150f6828e011dc8a6ca4a
Aaron Y <aiyota@outlook.com>
2022-02-21 14:41:56 -0800

    Move SiteThemeProvider to index.tsx.

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,21 @@ import App from "./pages/App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Word from "./pages/Word/Word";
 import { WordsProvider } from "./context/words/WordsProvider";
+import SiteThemeProvider from "./context/theme/SiteThemeProvider";
 
 ReactDOM.render(
   <React.StrictMode>
     <WordsProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/word" element={<Word />}>
-            <Route path=":wordId"></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <SiteThemeProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/word" element={<Word />}>
+              <Route path=":wordId"></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </SiteThemeProvider>
     </WordsProvider>
   </React.StrictMode>,
   document.getElementById("root"),
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,17 +2,13 @@ import * as React from "react";
 import { CssBaseline } from "@mui/material";
 import Container from "@mui/material/Container";
 import Words from "./Words/Words";
-import { WordsProvider } from "../context/words/WordsProvider";
-import SiteThemeProvider from "../context/theme/SiteThemeProvider";
 
 const App = () => {
   return (
-    <SiteThemeProvider>
-      <Container maxWidth="md">
-        <CssBaseline />
-        <Words />
-      </Container>
-    </SiteThemeProvider>
+    <Container maxWidth="md">
+      <CssBaseline />
+      <Words />
+    </Container>
   );
 };
 
